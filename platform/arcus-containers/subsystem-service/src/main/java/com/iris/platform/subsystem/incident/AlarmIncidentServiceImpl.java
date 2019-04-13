2a6b528e8fb85a6a542f6edcb84e69713bb4bf0c
Andrew Sorensen <andrew@andrewsorensen.net>
2019-04-12 18:17:03 -0700

    hard code ForkJoinPool, since I can't figure out how to inject threadpool.incident

--- platform/arcus-containers/subsystem-service/src/main/java/com/iris/platform/subsystem/incident/AlarmIncidentServiceImpl.java
+++ platform/arcus-containers/subsystem-service/src/main/java/com/iris/platform/subsystem/incident/AlarmIncidentServiceImpl.java
@@ -16,6 +16,7 @@
 package com.iris.platform.subsystem.incident;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -43,7 +44,7 @@ public class AlarmIncidentServiceImpl extends AbstractPlatformService {
    @Inject
    public AlarmIncidentServiceImpl(
          PlatformMessageBus platformBus,
-         @Named(NAME_EXECUTOR_POOL) ExecutorService executor,
+         ForkJoinPool executor,
          SubsystemExecutorResolver subsystemExecutorResolver,
          PlatformDispatcherFactory dispatcherFactory,
          GetAttributesHandler getAttributesHandler,
