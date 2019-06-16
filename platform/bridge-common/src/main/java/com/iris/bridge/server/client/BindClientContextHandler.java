68e54b7406a54dca6b2002832c607c23db00c80a
Andrew Sorensen <andrew@andrewsorensen.net>
2019-06-16 13:22:34 -0700

    Ignore authorization header for sessions when it's OAuth

--- platform/bridge-common/src/main/java/com/iris/bridge/server/client/BindClientContextHandler.java
+++ platform/bridge-common/src/main/java/com/iris/bridge/server/client/BindClientContextHandler.java
@@ -71,6 +71,9 @@ public class BindClientContextHandler extends ChannelInboundHandlerAdapter {
          logger.trace("Received HTTP request, determining context");
          try {
             String sessionId = extractSessionId((FullHttpRequest) msg);
+            if (sessionId == null) {
+               return;
+            }
             logger.debug("Extracted Session Id in BindClientContext: {}", sessionId);
             Client.bind(ctx.channel(), registry.load(sessionId));
             BridgeMdcUtil.bindHttpContext(registry, ctx.channel(), (FullHttpRequest) msg);
@@ -120,7 +123,7 @@ public class BindClientContextHandler extends ChannelInboundHandlerAdapter {
    }
 
    private String extractFromAuthHeader(String authHeader) {
-      return StringUtils.isEmpty(authHeader) ? null : authHeader;
+      return (StringUtils.isEmpty(authHeader) || authHeader.startsWith("Bearer")) ? null : authHeader;
    }
 }
 
